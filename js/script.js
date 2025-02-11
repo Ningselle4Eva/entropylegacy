8612e8b7a943483b3d2821a57a83d356e480d60a
Ningselle4Eva <t22995825@gmail.com>
2025-02-11 16:35:02 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-const channels = [
-    {
-        name: "GLOBAL TREKKER",
-        src: "https://linearjitp-playback.astro.com.my/dash-wv/linear/5094/default_primary.mpd",
-        key: "92d34a84fae8e54de0a829629941be10:2fb39ab3f55333d5fa3e830ebf99ec16",
-        drm: "clearkey"
-    },
-    {
-        name: "LOVE NATURE",
-        src: "https://linearjitp-playback.astro.com.my/dash-wv/linear/5096/default_primary.mpd",
-        key: "168bd815468639fe4528b4bf0141f310:775a654c0610811ad022329a68884de1",
-        drm: "clearkey"
-    },
-];
-
 function handleKeyPress(event) {
     if (event.key === 'Enter') {
         checkPassword();
@@ -41,6 +26,21 @@ function togglePassword() {
         toggleButton.textContent = "Show";
     }
 }
+
+const channels = [
+    {
+        name: "GLOBAL TREKKER",
+        src: "https://linearjitp-playback.astro.com.my/dash-wv/linear/5094/default_primary.mpd",
+        key: "92d34a84fae8e54de0a829629941be10:2fb39ab3f55333d5fa3e830ebf99ec16",
+        drm: "clearkey"
+    },
+    {
+        name: "LOVE NATURE",
+        src: "https://linearjitp-playback.astro.com.my/dash-wv/linear/5096/default_primary.mpd",
+        key: "168bd815468639fe4528b4bf0141f310:775a654c0610811ad022329a68884de1",
+        drm: "clearkey"
+    },
+];
 
 document.addEventListener('DOMContentLoaded', async () => {
     const videoElement = document.getElementById('video');
